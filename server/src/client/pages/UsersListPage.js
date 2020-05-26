34ec947373cf1052c71d5e1f7ba627435d53ab3f
Alexei Spirit <alexeispirit@gmail.com>
2020-05-26 22:58:03 +0500

    51-refactoring-to-pages

--- server/src/client/pages/UsersListPage.js
+++ server/src/client/pages/UsersListPage.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 
 import { fetchUsers } from "../actions";
 
-class UsersList extends Component {
+class UsersListPage extends Component {
   componentDidMount() {
     this.props.fetchUsers();
   }
@@ -36,4 +36,4 @@ function loadData(store) {
 
 export { loadData };
 
-export default connect(mapStateToProps, { fetchUsers })(UsersList);
+export default connect(mapStateToProps, { fetchUsers })(UsersListPage);
