c38052fb565708a1851279f21fab48440cb64752
Alexei Spirit <alexeispirit@gmail.com>
2020-05-27 23:18:48 +0500

    77-admins-action-creator-and-reducer

--- server/public/bundle.js
+++ server/public/bundle.js
@@ -2902,6 +2902,7 @@ Object.defineProperty(exports, "__esModule", {
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
 var FETCH_USERS = exports.FETCH_USERS = "FETCH_USERS";
+var FETCH_ADMINS = exports.FETCH_ADMINS = "FETCH_ADMINS";
 var FETCH_CURRENT_USER = exports.FETCH_CURRENT_USER = "FETCH_CURRENT_USER";
 
 var fetchUsers = exports.fetchUsers = function fetchUsers() {
@@ -2968,6 +2969,40 @@ var fetchCurrentUser = exports.fetchCurrentUser = function fetchCurrentUser() {
 
     return function (_x4, _x5, _x6) {
       return _ref2.apply(this, arguments);
+    };
+  }();
+};
+
+var fetchAdmins = exports.fetchAdmins = function fetchAdmins() {
+  return function () {
+    var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3(dispatch, getState, api) {
+      var res;
+      return regeneratorRuntime.wrap(function _callee3$(_context3) {
+        while (1) {
+          switch (_context3.prev = _context3.next) {
+            case 0:
+              _context3.next = 2;
+              return api.get("/admins");
+
+            case 2:
+              res = _context3.sent;
+
+
+              dispatch({
+                type: FETCH_ADMINS,
+                payload: res.data
+              });
+
+            case 4:
+            case "end":
+              return _context3.stop();
+          }
+        }
+      }, _callee3, undefined);
+    }));
+
+    return function (_x7, _x8, _x9) {
+      return _ref3.apply(this, arguments);
     };
   }();
 };
@@ -39956,11 +39991,16 @@ var _authReducer = __webpack_require__(487);
 
 var _authReducer2 = _interopRequireDefault(_authReducer);
 
+var _adminsReducer = __webpack_require__(488);
+
+var _adminsReducer2 = _interopRequireDefault(_adminsReducer);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = (0, _redux.combineReducers)({
   users: _usersReducer2.default,
-  auth: _authReducer2.default
+  auth: _authReducer2.default,
+  admins: _adminsReducer2.default
 });
 
 /***/ }),
@@ -40012,6 +40052,31 @@ exports.default = function () {
 };
 
 var _actions = __webpack_require__(77);
+
+/***/ }),
+/* 488 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _actions = __webpack_require__(77);
+
+exports.default = function () {
+  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+  var action = arguments[1];
+
+  switch (action.type) {
+    case _actions.FETCH_ADMINS:
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 /***/ })
 /******/ ]);
--- server/src/client/actions/index.js
+++ server/src/client/actions/index.js
@@ -1,4 +1,5 @@
 export const FETCH_USERS = "FETCH_USERS";
+export const FETCH_ADMINS = "FETCH_ADMINS";
 export const FETCH_CURRENT_USER = "FETCH_CURRENT_USER";
 
 export const fetchUsers = () => async (dispatch, getState, api) => {
@@ -15,6 +16,15 @@ export const fetchCurrentUser = () => async (dispatch, getState, api) => {
 
   dispatch({
     type: FETCH_CURRENT_USER,
+    payload: res.data,
+  });
+};
+
+export const fetchAdmins = () => async (dispatch, getState, api) => {
+  const res = await api.get("/admins");
+
+  dispatch({
+    type: FETCH_ADMINS,
     payload: res.data,
   });
 };
