e6c69a5cf3c7d828c27c8a32af7a8fe6e2805914
Alexei Spirit <alexeispirit@gmail.com>
2020-05-27 16:50:42 +0500

    60-proxy-setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 import "babel-polyfill";
 import express from "express";
+import proxy from "express-http-proxy";
 import { matchRoutes } from "react-router-config";
 
 import Routes from "./client/Routes";
@@ -7,6 +8,16 @@ import renderer from "./helpers/renderer";
 import createStore from "./helpers/createStore";
 
 const app = express();
+
+app.use(
+  "/api",
+  proxy("http://react-ssr-api.herokuapp.com", {
+    proxyReqOptDecorator(opts) {
+      opts.header["x-forward-host"] = "localhost:3000";
+      return opts;
+    },
+  })
+);
 
 app.use(express.static("public"));
 
