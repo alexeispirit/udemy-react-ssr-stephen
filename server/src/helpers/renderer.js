85ce329c512a9d0b2aedf954991ac8bb8067e7b8
Alexei Spirit <alexeispirit@gmail.com>
2020-05-27 23:14:25 +0500

    75-StaticRouter-context

--- server/src/helpers/renderer.js
+++ server/src/helpers/renderer.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom";
-import { Provider } from "react-redux";
+import { Provider, connect } from "react-redux";
 import { renderRoutes } from "react-router-config";
 import serialize from "serialize-javascript";
 
 import Routes from "../client/Routes";
 
-export default (req, store) => {
+export default (req, store, context) => {
   const content = renderToString(
     <Provider store={store}>
-      <StaticRouter location={req.path} context={{}}>
+      <StaticRouter location={req.path} context={context}>
         <div>{renderRoutes(Routes)}</div>
       </StaticRouter>
     </Provider>
